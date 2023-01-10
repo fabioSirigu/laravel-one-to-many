d3e3bae58310706cef92620a5e7de57a97070d50
naz1ne <fabio.sirigu.1995@gmail.com>
2023-01-10 14:33:04 +0100

    add model migration controller and db

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+            $project->title = $faker->sentence(3);
+            $project->description = $faker->text();
+            $project->date = $faker->date();
+            $project->save();
+        }
+    }
+}
